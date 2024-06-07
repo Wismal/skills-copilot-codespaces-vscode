1d8f683617bb19b285f70292bba83fc268a5f420
Alberto Wisniewski Mata <118727740+Wismal@users.noreply.github.com>
2024-06-07 10:41:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.createElement("div");
+  skills.innerHTML = "skills";
+  member.appendChild(skills);
+}
